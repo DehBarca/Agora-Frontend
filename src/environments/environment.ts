b415765e35bb1374c73c0435c7c10d19578ae078
DehBarca <dialebar@gmail.com>
2026-05-13 22:07:58 -0600

    fix google url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl:'https://agora-backend-h1y7.onrender.com',
+    apiUrl:'https://agora-backend-h1y7.onrender.com/',
     googleClientId: '1047061455988-meqjg26ejfk132sk72e16bvpks1r40mf.apps.googleusercontent.com'
 };
