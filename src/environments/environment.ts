640011a8839dfae116f70476f3689c03a8ff9fe1
DehBarca <dialebar@gmail.com>
2026-05-13 21:44:47 -0600

    Se cambia ruta de backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,4 @@
-interface RuntimeEnvironment {
-    googleClientId?: string;
-}
-
-const runtimeEnvironment =
-    typeof window !== 'undefined'
-        ? (window as Window & { __AGORA_ENV__?: RuntimeEnvironment }).__AGORA_ENV__
-        : undefined;
-
 export const environment = {
-    apiUrl: 'https://api.agora.diego-barraza-dev.com/',
-    googleClientId: runtimeEnvironment?.googleClientId ?? ''
+    apiUrl:'https://agora-backend-h1y7.onrender.com',
+    googleClientId: '1047061455988-meqjg26ejfk132sk72e16bvpks1r40mf.apps.googleusercontent.com'
 };
